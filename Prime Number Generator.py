1dfca1be8b15745d3c489c5b117b8f563d04dc36
Snickersman6 <tylerjay1996@hotmail.com>
2019-12-28 21:19:59 -0600

    Add files via upload

--- Prime Number Generator.py
+++ Prime Number Generator.py
@@ -1,13 +1,27 @@
-num = 9999999
-i = 1
+# this is the number it will start at
+num = 1
+
+# this is the end of the range of number it checks
+fin = 90
 run = True
 
+
 while run:
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                print(num, 'is not prime')
+    if num == 1: # checks if number is equal to 1
+        print(num, 'is neither prime nor factorable')
+        num += 1
+    if num > 1:  # if the number is greater than one
+        while run:  # runs the loop
+            if num <= fin:  # checks if the number is lower than the final number
+                for i in range(2, num): # checks if number is prime
+                    if (num % i) == 0:
+                        if num <= fin:
+                            print(num, 'is not prime')
+                            num = num + 1
+                        else:
+                            pass
+                if num < fin:  # if the number is not prime and less than the final number
+                    print(num, 'is prime')
                 num = num + 1
-        else:
-            print(num, 'is prime')
-        num = num + 1
+            elif num > fin:  # if the number is larger than the final number this ends the loops
+                run = False
